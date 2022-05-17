8e000e070ca98aa4c48ebd1b717a704b74d65412
Andari757 <matviyskrypka@gmail.com>
2022-05-17 15:22:27 +0300

    1

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,16 +1,17 @@
 import styles from "./style.module.css";
 import PropTypes from 'prop-types';
 export function Statistics({ title, stats }) {
+    const elements = stats.map(stat => (
+        <li key={stat.id} className={styles.statistics__item}>
+            <span className={styles.statistics__label}>{stat.label}</span>
+            <span className={styles.statistics__percentage}>{stat.percentage}%</span>
+        </li>
+    ))
     return (
         <section className={styles.statistics}>
             {title && (<h2 className={styles.statistics__title}>{title}</h2>)}
             <ul className={styles.statistics__list}>
-                {stats.map(stat => (
-                    <li key={stat.id} className={styles.statistics__item}>
-                        <span className={styles.statistics__label}>{stat.label}</span>
-                        <span className={styles.statistics__percentage}>{stat.percentage}%</span>
-                    </li>
-                ))}
+                {elements}
             </ul>
         </section>
     );
--- src/components/transactionHistory/TransactionHistory.jsx
+++ src/components/transactionHistory/TransactionHistory.jsx
@@ -1,8 +1,14 @@
 import styles from "./style.module.css";
 import PropTypes from 'prop-types';
 export function TransactionHistory({ items }) {
+    const elements = items.map(item => (
+        <tr className={styles['transaction-history__row']} key={item.id}>
+            <td className={styles['transaction-history__row-element']}>{item.type}</td>
+            <td className={styles['transaction-history__row-element']}>{item.amount}</td>
+            <td className={styles['transaction-history__row-element']}>{item.currency}</td>
+        </tr>
+    ))
     return (
-
         <table className={styles['transaction-history']}>
             <thead className={styles['transaction-history__head']}>
                 <tr className={`${styles['transaction-history__row']} ${styles['transaction-history__row--head']}`}>
@@ -12,17 +18,9 @@ export function TransactionHistory({ items }) {
                 </tr>
             </thead>
             <tbody className={styles['transaction-history__body']}>
-                {items.map(item => (
-                    <tr className={styles['transaction-history__row']} key={item.id}>
-                        <td className={styles['transaction-history__row-element']}>{item.type}</td>
-                        <td className={styles['transaction-history__row-element']}>{item.amount}</td>
-                        <td className={styles['transaction-history__row-element']}>{item.currency}</td>
-                    </tr>
-
-                ))}
+                {elements}
             </tbody>
         </table >
-
     );
 }
 TransactionHistory.defaultProps = {
