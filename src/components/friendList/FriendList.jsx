0ca7fe8ee4f38ebcd03adc296ca32fd8a270e70d
Andari757 <matviyskrypka@gmail.com>
2022-05-17 13:51:37 +0300

    1

--- src/components/friendList/FriendList.jsx
+++ src/components/friendList/FriendList.jsx
@@ -1,16 +1,30 @@
 import styles from "./style.module.css";
+import PropTypes from 'prop-types';
 import { FriendListItem } from "./friendListItem/FriendListItem";
-export function FriendList(friends) {
+export function FriendList({ friends }) {
     return (
         <ul className={styles['friend-list']}>
             {friends.map(friend => (
-                <FriendListItem
-                    friends={friend}
+                < FriendListItem
+                    key={friend.id}
+                    friend={friend}
                 />
-            ))}
+            )
+            )}
         </ul >
 
     );
 }
 
 
+FriendList.defaultProps = {
+    friends: []
+}
+FriendList.propTypes = {
+    friends: PropTypes.arrayOf(PropTypes.shape({
+        avatar: PropTypes.string,
+        name: PropTypes.string,
+        isOnline: PropTypes.bool,
+        id: PropTypes.number
+    }))
+}
